8a991abeade1aa9e713c86c3fc641fb3eb2a53d0
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-10-22 19:06:37 -0300

    Entrega Trabajo Practico 1

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Grid, Paper, Box } from '@mui/material';
+import Card from '../Card/Card.js'
 
 const Dashboard = () => {
 	return (
 		<Grid container spacing={3}>
 			<Grid item xs={12}>
 				<Paper sx={{ p: 2 }}>
-					<Box>Dashboard</Box>
+					<Card/>
 				</Paper>
 			</Grid>
 		</Grid>
